819791989999c72e58821d64166d465cee7c268c
Gustavo Alejandro <llxsantaellaxll@gmail.com>
2022-06-27 12:36:02 -0400

    demo1

--- demo1.py
+++ demo1.py
@@ -1,19 +1,45 @@
-import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
 
 chromium_path = "./driver/chromedriver"
 service = Service(chromium_path)
 
+login = "https://rahulshettyacademy.com/loginpagePractise/"
+secretPassword = "learning"
+
 browser = webdriver.Chrome(service=service)
-browser.get('https://translate.google.cn/?hl=es&sl=es&tl=en&op=translate')
-element = browser.find_element(by='xpath', value='//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[3]/c-wiz[1]/span/span/div/textarea')
-element.send_keys("hola amigo")
-time.sleep(2)
-print('two seconds')
-resultElement = browser.find_element(by='xpath', value='//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[3]/c-wiz[2]/div[7]/div/div[1]/span[1]')
-print(resultElement.text)
+browser.implicitly_wait(4)
+
+browser.get(login)
+browser.find_element(by='xpath', value='/html/body/a').click()
+tabsOpened = browser.window_handles
+browser.switch_to.window(tabsOpened[1])
+
+element = browser.find_element(by='xpath', value='//*[@id="interview-material-container"]/div/div[2]/p[2]/strong/a')
+username = element.text[7:25] + 'sd'
+
+browser.close()
+
+browser.switch_to.window(tabsOpened[0])
+input_username = browser.find_element(by='id', value='username')
+password = browser.find_element(by='id', value="password")
+browser.find_element(by='id', value="terms").click()
+input_username.send_keys(username)
+password.send_keys(secretPassword)
+selectElement = browser.find_element(by='xpath', value='//*[@id="login-form"]/div[5]/select')
+select = Select(selectElement)
+select.select_by_visible_text('Teacher')
+browser.find_element(by='id', value="signInBtn").click()
+try:
+    wait = WebDriverWait(browser, 10)
+    wait.until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, ".alert-danger")))
+    print(browser.find_element(By.CSS_SELECTOR, ".alert-danger").text)
+except:
+    print('login')
+
 
 browser.close()
